79ce7722c9eacfd08533e2d1686a2b2947005b9e
STR8LINE <70938099+Str8line-data@users.noreply.github.com>
2021-01-03 16:59:15 +0530

    commit wheater app code by using context api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
+import React,{createContext,useState,useContext} from 'react';
 import './App.css';
 
-function App() {
+const WeatherContext = createContext({
+  cities: [],
+  addCity: (name, temperature) => { },
+});
+
+const CityList = (props) => {
+  const context = useContext(WeatherContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         My First React App deploy on github server live
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <table className="city-list">
+      <thead>
+        <tr>
+          <th>City</th>
+          <th>Temperature</th>
+        </tr>
+      </thead>
+      <tbody>
+        {context.cities.map((city, i) => (
+          <tr key={city.name}>
+            <td>{city.name}</td>
+            <td>{city.temperature}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const TemperatureAverage = (props) => {
+  const context = useContext(WeatherContext);
+  if (context.cities.length === 0) {
+    return (
+      <div>Add some cities to view their average temperatures.</div>
+    );
+  }
+  let total = 0;
+  for (const city of context.cities) {
+    total += city.temperature;
+  }
+  const avg = total / context.cities.length;
+  return (
+    <div>
+      The average is <b>{avg.toFixed(2)}</b> degrees Fahrenheit.
     </div>
+  );
+};
+
+const AddCityButton = (props) => {
+  const context = useContext(WeatherContext);
+  const [name, setName] = useState('');
+  const submit = () => {
+    context.addCity(name, Math.ceil(Math.random() * 10));
+    setName('');
+  };
+  return (
+    <div className="add-city-form">
+      <input className="input" value={name} onChange={(e) => setName(e.target.value)} />
+      <button className="input" onClick={submit}>Add</button>
+    </div>
+  );
+};
+
+
+function App() {
+  const [cities, setCities] = useState([]);
+
+  const addCity = (name, temperature) => {
+    const newCity = { name, temperature };
+    setCities(prevCities => [...prevCities, { name, temperature }]);
+  };
+
+  return (
+     <WeatherContext.Provider value={{
+      cities,
+      addCity,
+    }}>
+      <div className="city-overview">
+        <h2>Multi-Weather App</h2>
+        <CityList />
+        <AddCityButton />
+        <TemperatureAverage />
+      </div>
+    </WeatherContext.Provider>
   );
 }
 
